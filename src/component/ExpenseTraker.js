4582f05fc91fa06e8497bd8d357b3332ca396f1b
@betty2926 <betelhem.getachew2926@gmail.com>
2022-10-27 05:18:55 -0700

    separated the date to another component then styled it

--- src/component/ExpenseTraker.js
+++ src/component/ExpenseTraker.js
@@ -1,17 +1,14 @@
 import React from 'react'
+import ExpenseDate from './ExpenseDate'
 import './ExpenseTraker.css'
 
 function ExpenseTraker({ date, title, amount}) {
 
-    const month = JSON.stringify(date.toLocaleString('en-US', {month: 'long'}));
-    const day = JSON.stringify(date.toLocaleString('en-US'), {day: '2-digit'});
-    const year = JSON.stringify(date.getFullYear());
+    
 
   return (
     <div className='expense-item'>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+      <ExpenseDate date={date}/>
       <div className='expense-item__description'>
         <h2>{JSON.stringify(title)}</h2>
         <div className='expense-item__price'>{JSON.stringify(amount)}</div>
