d1f8998b18d043b3ce0298b6851f775910549af4
@betty2926 <betelhem.getachew2926@gmail.com>
2022-10-28 01:48:25 -0700

    file structure changed and styling added

--- src/App.js
+++ src/App.js
@@ -1,54 +1,40 @@
 import "./App.css";
-import ExpenseTraker from "./component/ExpenseTraker";
+import Expenses from './component/expenses/Expense';
 
 function App() {
   const expenses = [
     {
-      id: "e1",
-      title: "Toilet Paper",
+      id: 'e1',
+      title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: "e3",
-      title: "Car Insurance",
+      id: 'e3',
+      title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: "e4",
-      title: "New Desk (Wooden)",
+      id: 'e4',
+      title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
 
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
-    <div className="App">
-     {/* {expenses.map((values)=>(
-       <ExpenseTraker
-       title={values.title}
-       amount={values.amount}
-       date={values.date}
-     ></ExpenseTraker>
-     ))} */}
-     
-      <ExpenseTraker
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseTraker
-        title={expenses[3].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseTraker
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
